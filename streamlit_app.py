4983e920799f1e81ccd21c71b10b893deda24ea8
Dhipo Alam <dhipo.alam@outlook.com>
2024-06-28 15:39:36 +0700

    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def main():
     st.latex('c^2 = a^2+b^2')
     st.navigation([
         st.Page('streamlit_app.py', title='Home', icon=':material/home:'),
-        st.Page('pages/playgrounds.py', title='Demo', icon=':material/family_link:'),
+        st.Page('playgrounds.py', title='Demo', icon=':material/family_link:'),
     ])
 if __name__ == '__main__':
     main()
