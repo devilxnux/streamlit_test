149f3e340e4b263b8eac3b5aafcc47123fa03ae3
Dhipo Alam <dhipo.alam@outlook.com>
2024-06-28 11:24:49 +0700

    Change table to ag-grid

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_aggrid import AgGrid
 import pandas as pd
 df_data = pd.read_csv("sample_data/house_clean.csv")
 def main():
@@ -10,7 +11,7 @@ def main():
         ### Sub Subheader''')
     st.write('Some Phytagorean Equation:')
     st.latex('c^2 = a^2+b^2')
-    st.dataframe(df_data, height=160, use_container_width=True)
+    st.AgGrid(df_data, height=160, use_container_width=True)
     st.metric(label='Temperature', value='70 °C', delta='-1.2 °C')
 if __name__ == '__main__':
     main()
