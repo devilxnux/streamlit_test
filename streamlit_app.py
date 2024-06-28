b165bbf0a8ea7c560a1a8613e61e1b16cca0bf39
Dhipo Alam <dhipo.alam@outlook.com>
2024-06-28 10:03:55 +0700

    Test header, subheader, markdown, LaTEX

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
 def main():
-    st.header('Contoh Aplikasi Minimal')
-    st.write('Minimal Example')
+    st.header('This is Header')
+    st.subheader('This is Subheader')
+    st.markdown('''
+        # Rendering Markdown
+        ## Markdown Subheader
+        ### Sub Subheader''')
+    st.write('Some Phytagorean Equation:')
+    st.latex('c^2 = a^2+b^2')
 
 if __name__ == '__main__':
     main()
