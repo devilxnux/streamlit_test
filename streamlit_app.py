21f4e384eee5bbc9acf6e4ba366adcb3736acb9e
Dhipo Alam <dhipo.alam@outlook.com>
2024-06-28 11:15:01 +0700

    Tabel dari dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 def main():
     st.header('This is Header')
@@ -9,6 +10,8 @@ def main():
         ### Sub Subheader''')
     st.write('Some Phytagorean Equation:')
     st.latex('c^2 = a^2+b^2')
+    df_data = pd.read_csv("sample_data/california_housing_train.csv")
+    st.table(df_data)
 
 if __name__ == '__main__':
     main()
