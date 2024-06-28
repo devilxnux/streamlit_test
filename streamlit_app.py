1adf5e2c8589101c1c07e84cb85c8292ccd99bc6
Dhipo Alam <dhipo.alam@outlook.com>
2024-06-28 13:42:27 +0700

    Add text input, checkbox, radio

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,23 @@ def main():
     with tab_agg:
         AgGrid(df_data, height=160, use_container_width=True)
     st.metric(label='Temperature', value='70 °C', delta='-1.2 °C')
+    test_button = st.button('Hello!')
+    if test_button:
+        st.write('Hello too!')
+    text_nama = st.text_input('Siapa nama Anda?')
+    if str(text_nama).strip() != "":
+        st.write(f"Hai, {text_nama}!")
+    check_agree = st.checkbox('Apakah Anda setuju jika olahraga itu penting?')
+    if check_agree:
+        st.write('Anda setuju!')
+    else:
+        st.write('Ada TIDAK setuju!')
+    radio_nilai = st.radio('Berapa kali Anda olahraga dalam seminggu?', [1, 2, 3, 4, 5])
+    if radio_nilai > 2:
+        st.write('Luar biasa! Pertahankan kebiasaan baik Anda!')
+    if radio_nilai > 0:
+        st.write('Sudah bagus! Tapi dapat ditingkatkan!')
+    else:
+        st.write('Mulailah berolahraga hari ini!')
 if __name__ == '__main__':
     main()
