6e33df5817e5688ad78a60ddd16820a73113c2d2
Dhipo Alam <dhipo.alam@outlook.com>
2024-06-28 11:16:48 +0700

    Perbaiki tabel 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def main():
     st.write('Some Phytagorean Equation:')
     st.latex('c^2 = a^2+b^2')
     df_data = pd.read_csv("sample_data/house_clean.csv")
-    st.dataframe(df_data)
+    st.table(df_data)
 
 if __name__ == '__main__':
     main()
