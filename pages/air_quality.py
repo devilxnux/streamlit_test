a25206ccd3c5fd67b50141c69cbd8cdbb2926d7e
Dhipo Alam <dhipo.alam@outlook.com>
2024-07-01 15:26:00 +0700

    Add sliders

--- pages/air_quality.py
+++ pages/air_quality.py
@@ -1,4 +1,5 @@
 import pickle
+import pandas as pd
 import streamlit as st
 
 loaded_model = pickle.load(open('knn_model.pkl', mode='rb'))
@@ -17,5 +18,25 @@ st.subheader('Cuaca')
 with st.container(border=True):
     val_temp = st.slider('Temperatur', min_value=-10.0, max_value=40.0, step=0.1)
     val_humd = st.slider('Kelembaban', min_value=10.0, max_value=100.0, step=0.1)
-    val_wind
+    val_wind = st.slider('Kecepatan Angin', min_value=0.0, max_value=20.0, step=0.1)
+
+button_predict = st.button('Prediksi')
+
+if button_predict:
+    features = {
+    'AQI': val_aqi, 
+    'PM10': val_pm10, 
+    'PM2_5': val_pm25, 
+    'NO2': val_no2, 
+    'SO2': val_so2, 
+    'O3': val_o3, 
+    'Temperature': val_temp,
+    'Humidity': val_humd, 
+    'WindSpeed': val_wind, 
+    'RespiratoryCases': 12, 
+    'CardiovascularCases': 7,
+    'HospitalAdmissions': 6,
+    }
+    df_features = pd.DataFrame([features])
+    
     
